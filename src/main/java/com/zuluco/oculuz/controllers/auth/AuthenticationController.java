5823b74b7099a93578700eaa1687c35dcc5138bb
NISSAN-180SX-NISMO <semenaksin13@gmail.com>
2024-03-06 22:06:40 +0300

    fix some  auth

--- src/main/java/com/zuluco/oculuz/controllers/auth/AuthenticationController.java
+++ src/main/java/com/zuluco/oculuz/controllers/auth/AuthenticationController.java
@@ -2,10 +2,13 @@ package com.zuluco.oculuz.controllers.auth;
 
 import com.zuluco.oculuz.model.dtos.AuthenticationRequest;
 import com.zuluco.oculuz.model.dtos.AuthenticationResponse;
+import com.zuluco.oculuz.model.dtos.user.ErrorResponse;
 import com.zuluco.oculuz.model.entities.User;
 import com.zuluco.oculuz.model.services.JwtUtil;
 import com.zuluco.oculuz.model.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -26,21 +29,30 @@ public class AuthenticationController {
     private UserService userService;
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
-
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
+        System.out.println("im in createAuthenticationToken");
+        System.out.println(authenticationRequest);
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
             );
+            System.out.println("nice user!");
         }
         catch (BadCredentialsException e) {
-            throw new Exception("Incorrect username or password", e);
+            System.out.println("Incorrect username or password");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse("Incorrect username or password"));
         }
 
         final User user = userService.loadUserByUsername(authenticationRequest.getUsername());
 
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("User not found"));
+        }
+
         final String jwt = jwtTokenUtil.generateToken(user);
 
-        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwt);
+        return new ResponseEntity<>(new AuthenticationResponse(jwt), headers, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/zuluco/oculuz/model/dtos/user/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.zuluco.oculuz.model.dtos.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
