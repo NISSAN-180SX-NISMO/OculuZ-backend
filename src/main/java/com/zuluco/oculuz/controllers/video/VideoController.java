9ec3b899614ba5ef919872c2a8aa4da9f790b789
NISSAN-180SX-NISMO <semenaksin13@gmail.com>
2024-03-14 17:07:12 +0300

    some decompose

--- src/main/java/com/zuluco/oculuz/controllers/video/VideoController.java
+++ src/main/java/com/zuluco/oculuz/controllers/video/VideoController.java
@@ -1,7 +1,6 @@
 package com.zuluco.oculuz.controllers.video;
 
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
 import com.zuluco.oculuz.payload.request.InitiateUploadRequest;
 import com.zuluco.oculuz.payload.response.CompleteUploadResponse;
@@ -13,13 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -28,61 +24,32 @@ public class VideoController {
     @Autowired
     private VideoService videoService;
 
-    @Autowired
-    private AmazonS3 s3Client;
-
-    private static final String bucketName = "fd22a2a2-oculuz-media-storage";
-    private static String keyName = "video";
-
     private static final Logger logger = LoggerFactory.getLogger(VideoController.class);
-    List<PartETag> partETags = new ArrayList<>();
-
 
     @PostMapping("/init-upload")
     public ResponseEntity<UploadResponse> initUpload(@RequestBody InitiateUploadRequest request) {
         try {
-            keyName += "/" + request.getFileName();
-            InitiateMultipartUploadRequest initiateMultipartUploadRequest = new InitiateMultipartUploadRequest(bucketName, keyName);
-            InitiateMultipartUploadResult initiateMultipartUploadResult = s3Client.initiateMultipartUpload(initiateMultipartUploadRequest);
-
-            String uploadId = initiateMultipartUploadResult.getUploadId();
-
-
+            String uploadId = videoService.initUpload(request.getFileName()).getUploadId();
             logger.info("Upload initiated with ID: " + uploadId);
             return ResponseEntity.ok(new UploadResponse(uploadId, "Upload initiated successfully", 0));
         } catch (AmazonServiceException e) {
             logger.error("Error occurred while initiating upload", e);
-
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new UploadResponse(null, "Error occurred while initiating upload", 0));
         }
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<UploadResponse> handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("partNumber") int partNumber, @RequestParam("uploadId") String uploadId) {
+    public ResponseEntity<UploadResponse> handleFileUpload(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("partNumber") int partNumber,
+            @RequestParam("uploadId") String uploadId
+    ) {
         try {
-            InputStream inputStream = file.getInputStream();
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentLength(file.getSize());
-
-            UploadPartRequest uploadPartRequest = new UploadPartRequest()
-                    .withBucketName(bucketName)
-                    .withKey(keyName) // используйте имя файла, заданное при инициализации
-                    .withUploadId(uploadId)
-                    .withPartNumber(partNumber)
-                    .withInputStream(inputStream)
-                    .withPartSize(file.getSize());
-
-
-            // Загрузка части и получение результата
-            UploadPartResult uploadResult = s3Client.uploadPart(uploadPartRequest);
-            // Сохранение ETag в ваш список
-            partETags.add(uploadResult.getPartETag());
-
+            UploadPartResult uploadResult = videoService.handleFileUpload(file, partNumber, uploadId);
             logger.info("Part " + partNumber + " uploaded successfully");
             return ResponseEntity.ok(new UploadResponse(uploadId, "Part " + partNumber + " uploaded successfully", partNumber));
         } catch (IOException e) {
             logger.error("Error occurred while uploading part " + partNumber, e);
-
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new UploadResponse(uploadId, "Error occurred while uploading part " + partNumber, partNumber));
         }
     }
@@ -90,22 +57,12 @@ public class VideoController {
     @PostMapping("/complete-upload")
     public ResponseEntity<CompleteUploadResponse> completeMultipartUpload(@RequestParam("uploadId") String uploadId) {
         try {
-            CompleteMultipartUploadRequest compRequest = new CompleteMultipartUploadRequest(
-                    bucketName,
-                    keyName,
-                    uploadId,
-                    partETags);
-
-            CompleteMultipartUploadResult compResult = s3Client.completeMultipartUpload(compRequest);
+            CompleteMultipartUploadResult compResult = videoService.completeMultipartUpload(uploadId);
             logger.info("Загрузка завершена. Файл доступен по ключу: " + compResult.getKey());
-            this.partETags.clear();
             return ResponseEntity.ok(new CompleteUploadResponse(compResult.getLocation()));
         } catch (AmazonServiceException e) {
             logger.error("Error occurred while completing upload", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new CompleteUploadResponse(null));
         }
     }
-
-
-
 }
--- src/main/java/com/zuluco/oculuz/services/VideoService.java
+++ src/main/java/com/zuluco/oculuz/services/VideoService.java
@@ -4,10 +4,69 @@ import com.zuluco.oculuz.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class VideoService {
     @Autowired
-    private VideoRepository videoRepository;
+    private AmazonS3 s3Client;
 
+    private static final String bucketName = "fd22a2a2-oculuz-media-storage";
+    private static String keyName;
 
+    List<PartETag> partETags = new ArrayList<>();
+
+    private void clearStatement() {
+        partETags.clear();
+        keyName = "video";
+    }
+
+    public InitiateMultipartUploadResult initUpload(String fileName) {
+        clearStatement();
+        keyName += "/" + fileName;
+        InitiateMultipartUploadRequest initiateMultipartUploadRequest = new InitiateMultipartUploadRequest(bucketName, keyName);
+        return s3Client.initiateMultipartUpload(initiateMultipartUploadRequest);
+    }
+
+    public UploadPartResult handleFileUpload(MultipartFile file, int partNumber, String uploadId) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+
+        UploadPartRequest uploadPartRequest = new UploadPartRequest()
+                .withBucketName(bucketName)
+                .withKey(keyName)
+                .withUploadId(uploadId)
+                .withPartNumber(partNumber)
+                .withInputStream(inputStream)
+                .withPartSize(file.getSize());
+
+        UploadPartResult uploadResult = s3Client.uploadPart(uploadPartRequest);
+        partETags.add(uploadResult.getPartETag());
+
+        return uploadResult;
+    }
+
+    public CompleteMultipartUploadResult completeMultipartUpload(String uploadId) {
+        CompleteMultipartUploadRequest compRequest = new CompleteMultipartUploadRequest(
+                bucketName,
+                keyName,
+                uploadId,
+                partETags);
+
+        CompleteMultipartUploadResult compResult = s3Client.completeMultipartUpload(compRequest);
+        clearStatement();
+        return compResult;
+    }
 }
